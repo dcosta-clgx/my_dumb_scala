87b3190b3119090ad47488e529dca99095f6138c
Dave Costa <dcosta@corelogic.com>
2021-05-28 15:20:23 -0400

    Make DumbSparkTest do some calculations

--- src/main/scala/DumbSparkTest.scala
+++ src/main/scala/DumbSparkTest.scala
@@ -1,9 +1,35 @@
 import org.apache.spark.sql.SparkSession;
 
 object DumbSparkTest extends App {
+    def buildList(cardinality:Int): List[(Int,Int)] = {
+        val theList = (1 to cardinality).toList
+        theList.zipWithIndex
+    }
+
+    def mapper( pair:(Int,Int)): Int = {
+      pair._1 + pair._2
+    }
+
+    def doSparkStuff(input: List[(Int,Int)]): List[Int] = {
+        val sparkSession: SparkSession = SparkSession.builder.getOrCreate
+        val sparkContext: org.apache.spark.SparkContext = sparkSession.sparkContext
+
+        val inputRDD = sparkContext.makeRDD(input)
+
+        val outputRDD = inputRDD.map( mapper )
+
+        outputRDD.collect().toList
+    }
+
     println("This is main()")
 
-    val sparkSession: SparkSession = SparkSession.builder.getOrCreate
-    val sparkContext: org.apache.spark.SparkContext = sparkSession.sparkContext
+    val sumList = buildList(5 )
+
+    println("Running non-Spark")
+    val nonSparkResult = sumList.map(mapper)
+    for ( i <- nonSparkResult) println(i)
+
+    val sparkResult = doSparkStuff(sumList)
+    for (i <- sparkResult) println(i)
 
 }
